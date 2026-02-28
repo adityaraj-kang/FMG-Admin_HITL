b09bf8a7a4d62f961e20f3ca09fde9509dc55da4
Aditya Raj <adityaraj2942004@gmail.com>
2026-02-28 11:30:04 +0530

    chore: remove unused React import from AuthGuard

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useRouter, usePathname } from 'next/navigation'
 import { useAuth } from '@/context/AuthContext'
 
